276faab71622dedfe0cce7d7bd15eabb1cb7c615
khehla <frans.chabangu@icandy-research.co.za>
2023-09-15 03:19:23 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -163,7 +163,7 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-    
+        
     # List accounts 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
